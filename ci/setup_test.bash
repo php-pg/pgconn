6d24bcdabf8c9a9183cf100aeadfb551a4ac7ca6
Dmitry K <coder1994@gmail.com>
2022-02-08 01:36:47 +0300

    Style fix

--- ci/setup_test.bash
+++ ci/setup_test.bash
@@ -3,8 +3,8 @@ set -eux
 
 if [[ "${PGVERSION-}" =~ ^[0-9.]+$ ]]
 then
-#  sudo apt-get remove -y --purge postgresql libpq-dev libpq5 postgresql-client-common postgresql-common
-#  sudo rm -rf /var/lib/postgresql
+  sudo apt-get remove -y --purge postgresql libpq-dev libpq5 postgresql-client-common postgresql-common
+  sudo rm -rf /var/lib/postgresql
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sudo sh -c "echo deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main $PGVERSION >> /etc/apt/sources.list.d/postgresql.list"
   sudo apt-get update -qq
