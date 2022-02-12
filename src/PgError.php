9e54dea637c93bf2634261e5f164f0476250dbc9
Dmitry K <coder1994@gmail.com>
2022-02-12 04:28:55 +0300

    Small code smell improvements

--- src/PgError.php
+++ src/PgError.php
@@ -4,26 +4,6 @@ declare(strict_types=1);
 
 namespace PhpPg\PgConn;
 
-class PgError
+class PgError extends AbstractPgError
 {
-    public function __construct(
-        public string $severity,
-        public string $sqlState,
-        public string $message,
-        public string $detail,
-        public string $hint,
-        public int $position,
-        public int $internalPosition,
-        public string $internalQuery,
-        public string $where,
-        public string $schemaName,
-        public string $tableName,
-        public string $columnName,
-        public string $dataTypeName,
-        public string $constraintName,
-        public string $file,
-        public int $line,
-        public string $routine,
-    ) {
-    }
 }
